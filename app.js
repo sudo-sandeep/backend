0bf01873c59a89c662ea7e4b200ae2e146e269fe
Sandeep <notsandeepkumar@gmail.com>
2024-11-17 23:36:22 +0530

    cors option alter

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: ["http://localhost:5173"],
-        credentials: true,
-        methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+const corsOptions = {
+    origin: "*",
+    methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
