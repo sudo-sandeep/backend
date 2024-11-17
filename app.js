b8c1e87f72a8cecd859a389cfc5c23b3e103e347
Sandeep <notsandeepkumar@gmail.com>
2024-11-17 23:12:19 +0530

    cors modified

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ import todoRouter from "./routes/todo.js";
 import userRouter from "./routes/user.js";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({origin: 'http://localhost:5173',}));
+app.use(cors({ origin: ["http://localhost:5173", "*"] }));
 app.use(cookieParser());
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.get("/",(req,res) => {
-    res.send("<h1>Server is up and running!</h1>")
-})
-app.get("/api/v1/",(req,res) => {
-    res.send("<h1>API's are working!</h1>")
-})
+app.get("/", (req, res) => {
+    res.send("<h1>Server is up and running!</h1>");
+});
+app.get("/api/v1/", (req, res) => {
+    res.send("<h1>API's are working!</h1>");
+});
 
-app.use("/api/v1/todo/",todoRouter)
-app.use("/api/v1/user/",userRouter)
-app.use("*",(req,res) => {
-    res.status(404).send("<h1>Page not found</h1>")
-})
+app.use("/api/v1/todo/", todoRouter);
+app.use("/api/v1/user/", userRouter);
+app.use("*", (req, res) => {
+    res.status(404).send("<h1>Page not found</h1>");
+});
 
-export {app}
+export { app };
