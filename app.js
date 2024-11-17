32c52ca36fcda56928ee21cc60c11a22dd388824
Sandeep <notsandeepkumar@gmail.com>
2024-11-17 23:17:31 +0530

    cors fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", "*"] }));
+app.use(cors({ origin: ["http://localhost:5173"],credentials: true }));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
