d56a7cf353d53217ee61d56ec6f5a284931adafe
Sandeep <notsandeepkumar@gmail.com>
2024-11-17 23:39:41 +0530

    cors update

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const corsOptions = {
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
+app.options('*', cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
